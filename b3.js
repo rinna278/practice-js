3f9be27b3ae561a06b1e541436f87e9f7a77f9ac
rinna278 <thuholuc@gmail.com>
2025-04-22 13:28:57 +0700

    cm

--- b3.js
+++
@@ -1,9 +0,0 @@
-function sevenBoom(arr) {
-    if (arr.toString().includes("7"))
-      return "Boom!";
-    return "there is no 7 in the array";
-  }
-  
-  console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7])); // "Boom!"
-  console.log(sevenBoom([8, 6, 33, 100]));       // "there is no 7 in the array"
-  console.log(sevenBoom([2, 55, 60, 97, 86]));   // "Boom!"
